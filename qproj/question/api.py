63f86f3008294be1d04735ebff009d3e647c4a63
Sagynysh <sagynysh.kenel@ce.sdu.edu.kz>
2015-03-12 14:47:31 +0600

    Update api.py
    fix use in

--- qproj/question/api.py
+++ qproj/question/api.py
@@ -4,9 +4,9 @@ from tastypie.authorization import Authorization
 from tastypie import fields
 
 class PostResource(ModelResource):
-    author = fields.CharField(attribute="author", use_in="list");
-    title = fields.CharField(attribute="title", use_in="list");
-    text = fields.CharField(attribute="text", use_in="list");
+    author = fields.CharField(attribute="author");
+    title = fields.CharField(attribute="title");
+    text = fields.CharField(attribute="text");
     comment=fields.ToManyField('qproj.question.api.CommentResource','comment_set',null=True,use_in="detail",full=True);
 
     class Meta:
